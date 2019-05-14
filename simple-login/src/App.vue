1b3b5645ae4fef28f0880d8981197a5eb426167e
Andrés Rodríguez <30989783+arodr10@users.noreply.github.com>
2019-05-13 20:41:41 -0500

    Add files via upload

---
+++ simple-login/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm8 md6 lg5 xl4>
+        <v-card class="elevation-6">
+          <v-toolbar color="primary" dark card>
+            <v-toolbar-title>
+              Ingresa tus Credenciales
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field label="Email"></v-text-field>
+            <v-text-field label="Password"></v-text-field>
+          </v-card-text>
+          <v-card-text>
+            <v-layout justify-end>
+              <v-btn color="primary" @click="enviarInformacion">Ingresar</v-btn>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
